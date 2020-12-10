19d3b3f5b11fecef1b027c06ef34815044075142
Thor Summerall <thor.summerall@gmail.com>
2020-12-10 15:39:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,17 @@ class Song
     @@all
   end
 
-
+  #Michael Jackson - Black or White - pop.mp3
   def self.new_by_filename(filename)
+    song_artist = file_name.split(" - ")[0]
+    song_title = filename.split(" - ")
+    self.new(song_title)
 
+    new_song = self.all.find{|song_instance| song_instance.name == song_title}
+    new_song.artist_name = song_artist 
+    artist = Artist.find_or_create_by_name(song_artist)
+    artist.songs << new_song 
+    new_song 
   end
 
   def artist_name=(name)
